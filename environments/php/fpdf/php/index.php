d7635d4e7d0b9162e39e8e02fd1a99173cb79399
vandalieu <vandalieu@vandalieuubuntu.myguest.virtualbox.org>
2024-01-18 14:25:59 -0500

    Get rid of temp files in mpdf

--- environments/php/fpdf/php/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$mpdf = new \Mpdf\Mpdf(['tempDir' => __DIR__ . '/temp']);
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output();
-
-?>
