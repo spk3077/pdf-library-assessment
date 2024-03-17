805f8fce68da06effde4616662a058eda25fec5e
vandalieu <vandalieu@vandalieuubuntu.myguest.virtualbox.org>
2024-03-16 23:11:16 -0400

    IText Springboot Base code was added

---
+++ environments/itext/springboot/src/main/java/csec/pla/IText/ITextApplication.java
@@ -0,0 +1,13 @@
+package csec.pla.IText;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITextApplication.class, args);
+	}
+
+}
---
+++ environments/itext/springboot/src/test/java/csec/pla/IText/ITextApplicationTests.java
@@ -0,0 +1,13 @@
+package csec.pla.IText;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
