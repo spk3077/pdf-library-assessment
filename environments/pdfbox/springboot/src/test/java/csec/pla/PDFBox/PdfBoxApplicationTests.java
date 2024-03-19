13e3000a2f6efef1565751434d52f47eb1369217
vandalieu <vandalieu@vandalieuubuntu.myguest.virtualbox.org>
2024-03-18 23:04:20 -0400

    Working on ExploitController of PDFBox

---
+++ environments/pdfbox/springboot/src/test/java/csec/pla/PDFBox/PdfBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package csec.pla.PDFBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
