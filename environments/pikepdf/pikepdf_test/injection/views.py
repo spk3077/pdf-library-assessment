8cdfcb9bbbd6802583e151735e14a226ac862418
vandalieu <vandalieu@vandalieuubuntu.myguest.virtualbox.org>
2024-03-23 03:10:00 -0400

    PikePDF injection exploit complete

--- environments/pikepdf/pikepdf_test/injection/views.py
+++ environments/pikepdf/pikepdf_test/injection/views.py
@@ -4,45 +4,109 @@ from django.http import HttpResponse
 import sys
 
 import pikepdf
-from pikepdf import Pdf
-from io import BytesIO
-from reportlab.pdfgen import canvas
+from pikepdf import Array, Pdf, Stream
 
 sys.path.insert(0, "/usr/src/app")
 from py_payloads import escape_seq
 
-def generate_stamp(msg, xy):
-    x, y = xy
-    buf = BytesIO()
-    c = canvas.Canvas(buf, bottomup=0)
-    c.setFontSize(16)
-    c.setFillColorCMYK(0, 0, 0, 0, alpha=0.7)
-    c.rect(194, 5, 117, 17, stroke=1, fill=1)
-    c.setFillColorCMYK(0, 0, 0, 100, alpha=0.7)
-    c.drawString(x, y, msg)
-    c.save()
-    buf.seek(0)
-    return buf
-
-
 def index(request):
+    pikepdf.settings.set_flate_compression_level(0)
+
+    # Author
+    count = 0
+    for seq in escape_seq:
+        pdf = Pdf.new()
+        pdf.add_blank_page()
+
+        with pdf.open_metadata() as meta:
+            meta['pdf:Author'] = seq
+        
+        # Test
+        new_content = Stream(pdf, b'BT /F1 12 Tf 50 50 Td (DOGTEST) Tj ET')
+        pdf.pages[0].contents_add(new_content)
+
+        pdf.save("/pdfs/author" + str(count) + ".pdf")
+        count += 1
+
+    # Title
+    count = 0
+    for seq in escape_seq:
+        pdf = Pdf.new()
+        pdf.add_blank_page()
+
+        with pdf.open_metadata() as meta:
+            meta['dc:title'] = seq
+        
+        # Test
+        new_content = Stream(pdf, b'BT /F1 12 Tf 50 50 Td (DOGTEST) Tj ET')
+        pdf.pages[0].contents_add(new_content)
+
+        pdf.save("/pdfs/title" + str(count) + ".pdf")
+        count += 1
+
+    # Author
+    count = 0
+    for seq in escape_seq:
+        pdf = Pdf.new()
+        pdf.add_blank_page()
+
+        with pdf.open_metadata() as meta:
+            meta['pdf:Creator'] = seq
+
+        # Test
+        new_content = Stream(pdf, b'BT /F1 12 Tf 50 50 Td (DOGTEST) Tj ET')
+        pdf.pages[0].contents_add(new_content)
+
+        pdf.save("/pdfs/creator" + str(count) + ".pdf")
+        count += 1
+
+    # Producer
+    count = 0
+    for seq in escape_seq:
+        pdf = Pdf.new()
+        pdf.add_blank_page()
+
+        with pdf.open_metadata() as meta:
+            meta['pdf:Producer'] = seq
+
+        # Test
+        new_content = Stream(pdf, b'BT /F1 12 Tf 50 50 Td (DOGTEST) Tj ET')
+        pdf.pages[0].contents_add(new_content)
+
+        pdf.save("/pdfs/producer" + str(count) + ".pdf")
+        count += 1
+
+    # Keywords
+    count = 0
+    for seq in escape_seq:
+        pdf = Pdf.new()
+        pdf.add_blank_page()
+
+        with pdf.open_metadata() as meta:
+            meta['pdf:Keywords'] = seq
+
+        # Test
+        new_content = Stream(pdf, b'BT /F1 12 Tf 50 50 Td (DOGTEST) Tj ET')
+        pdf.pages[0].contents_add(new_content)
+
+        pdf.save("/pdfs/keywords" + str(count) + ".pdf")
+        count += 1
+
     # Text
     count = 0
     for seq in escape_seq:
-        stamp = generate_stamp(seq, (200, 20))
-        with pikepdf.open(stamp) as pdf_text:
-            pdf = Pdf.new()
-            pdf.add_blank_page()
-            formx_text = pdf.copy_foreign(pdf_text.pages[0].as_form_xobject())
+        pdf = Pdf.new()
+        pdf.add_blank_page()
+      
+        new_content = Stream(pdf, b'BT /F1 12 Tf 100 100 Td (' + seq.encode("utf-8") + b') Tj ET')
+        pdf.pages[0].contents_add(new_content)
 
-            for i in range(len(pdf.pages)):
-                formx_page = pdf.pages[i]
-                formx_name = formx_page.add_resource(formx_text, pikepdf.Name.XObject)
-                stamp_text = pdf.make_stream(b'q 1 0 0 1 0 0 cm %s Do Q' % formx_name)
+        # Test
+        new_content = Stream(pdf, b'BT /F1 12 Tf 50 50 Td (DOGTEST) Tj ET')
+        pdf.pages[0].contents_add(new_content)
 
-                pdf.pages[i].contents_add(stamp_text)
-            pdf.save("/pdfs/text" + str(seq) + ".pdf")
-            count += 1
+        pdf.save("/pdfs/text" + str(count) + ".pdf")
+        count += 1
 
     # URI
     count = 0
@@ -56,11 +120,18 @@ def index(request):
             Border=[0, 0, 1],
             A=pikepdf.Dictionary(
                 S=pikepdf.Name('/URI'),
-                URI=seq
+                URI="seq"
             )
         )
-        pdf.pages[0].add_annotation(link_annotation)
-        pdf.save("/pdfs/uri" + str(seq) + ".pdf")
+
+        pdf.pages[0]['/Annots'] = Array()
+        pdf.pages[0]['/Annots'].append(link_annotation)
+
+        # Test
+        new_content = Stream(pdf, b'BT /F1 12 Tf 50 50 Td (DOGTEST) Tj ET')
+        pdf.pages[0].contents_add(new_content)
+
+        pdf.save("/pdfs/uri" + str(count) + ".pdf")
         count += 1
 
     return HttpResponse("INJECTION")
